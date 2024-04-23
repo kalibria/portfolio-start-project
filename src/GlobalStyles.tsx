238a112c965d1c1cae4267c7221b6763f5dc75c5
Maria Kazakova <mariakazakova92@gmail.com>
2024-04-23 12:18:22 +0300

    add favicon

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -5,7 +5,16 @@ export const GlobalStyles = createGlobalStyle`
   *::before,
   *::after {
     margin:0;
-
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  li {
+    text-decoration: none;
+  }
+  
+  a {
+    text-decoration: none;
   }
 `
 
