79a0b975bb22832f336803dac75ad8cf0ec0cdac
Maria Kazakova <mariakazakova92@gmail.com>
2024-05-15 12:17:01 +0300

    add open / close mobile menu

--- src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
+++ src/layout/header/headerMenu/mobileMenu/MobileMenu.tsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {S} from '../HeaderMenu_Styles'
 import {MyButton} from "../../../../components/button/Button";
 import {Menu} from "../menu/Menu";
 
 export const MobileMenu: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const onClickButton = () => {
+        setIsOpen(!isOpen)
+    }
 
   return (
       <S.MobileMenu>
-          <S.BurgerButton isOpen={false}>
+          <S.BurgerButton isOpen={isOpen} onClick={onClickButton}>
               <span/>
           </S.BurgerButton>
-          <S.MobilePopup isOpen={false}>
+          <S.MobilePopup isOpen={isOpen} onClick = {() => setIsOpen(false)}>
               <S.StyledMenu>
                   <Menu/>
               </S.StyledMenu>
