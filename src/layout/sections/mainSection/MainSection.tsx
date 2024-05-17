6bb955fe8076e110d6c996794d57fbb973889a5f
Maria Kazakova <mariakazakova92@gmail.com>
2024-05-17 10:59:39 +0300

    add modal window by clicking on email me button

--- src/layout/sections/mainSection/MainSection.tsx
+++ src/layout/sections/mainSection/MainSection.tsx
@@ -7,28 +7,38 @@ import {FlexWrapper} from 'src/components/wrapper/FlexWrapper';
 // import photo from '../../../assets/images/myphoto.webp';
 import photo from 'src/assets/images/photo.webp'
 import {S} from './MainSection_Styled'
+import {ModalWindow} from "../../../components/modalWindow/ModalWindow";
 
-export const MainSection:React.FC = () => {
-  return (
-    <S.MainSection id={"home"}>
-      <S.Background backgroundColor={theme.colors.primaryColor}/>
-      <S.Background backgroundImage={photo}/>
-      <S.MainInfo>
-        <Container>
-          <FlexWrapper direction={'column'}>
-            <S.MainSectionText>Hello!</S.MainSectionText>
-            <S.MainSectionText>I’m Zarror Nibors</S.MainSectionText>
-            <S.MainDescription>
-              <StyledDescription>I’am freelance <S.Title> web developer</S.Title> based in Indonesia who loves to craft attractive design experiences for the web.</StyledDescription>
-              <S.ButtonsWrapper>
-                <MyButton text={'Email me'} iconId={"envelope"} padding={"20px 43px"} backgroundColor={"rgba(103, 108, 219, 1)"}/>
-                <MyButton as="a" href="#" text={'Download CV'} iconId={"download"} padding={"10px 20px"} backgroundColor={"transparent"} textDecoration={"underline"}/>
-              </S.ButtonsWrapper >
-            </S.MainDescription>
-          </FlexWrapper>
-        </Container>
+export const MainSection: React.FC = () => {
+    const [isOpenModalWindow, setIsOpenModalWindow] = React.useState(false);
+    const openModalWindow = () => {
+        setIsOpenModalWindow(true);
+    }
 
-      </S.MainInfo>
-    </S.MainSection>
-  );
+    return (
+        <S.MainSection id={"home"}>
+            <S.Background backgroundColor={theme.colors.primaryColor}/>
+            <S.Background backgroundImage={photo}/>
+            <S.MainInfo>
+                <Container>
+                    <FlexWrapper direction={'column'}>
+                        <S.MainSectionText>Hello!</S.MainSectionText>
+                        <S.MainSectionText>I’m Zarror Nibors</S.MainSectionText>
+                        <S.MainDescription>
+                            <StyledDescription>I’am freelance <S.Title> web developer</S.Title> based in Indonesia who
+                                loves to craft attractive design experiences for the web.</StyledDescription>
+                            <S.ButtonsWrapper>
+                                <MyButton text={'Email me'} iconId={"envelope"} padding={"20px 43px"}
+                                          backgroundColor={"rgba(103, 108, 219, 1)"} onClick={openModalWindow}/>
+                                <MyButton as="a" href="#" text={'Download CV'} iconId={"download"} padding={"10px 20px"}
+                                          backgroundColor={"transparent"} textDecoration={"underline"}/>
+                                <ModalWindow isOpen={isOpenModalWindow} setIsOpen={setIsOpenModalWindow}/>
+                            </S.ButtonsWrapper>
+                        </S.MainDescription>
+                    </FlexWrapper>
+                </Container>
+
+            </S.MainInfo>
+        </S.MainSection>
+    );
 };
