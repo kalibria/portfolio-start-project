acdb02e34b6f6f408515747b0187e0a583c79b06
Maria Kazakova <mariakazakova92@gmail.com>
2024-11-25 16:42:39 +0300

    added animation to the Connect component

--- src/layout/sections/connect/Connect.tsx
+++ src/layout/sections/connect/Connect.tsx
@@ -4,12 +4,27 @@ import {Form} from 'src/components/form/Form';
 import {Slogan} from 'src/layout/sections/connect/Slogan';
 import {FlexWrapper} from "../../../components/wrapper/FlexWrapper";
 import {S} from './Connect_Styles';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 
 export const Connect: React.FC = () => {
+    const ref = React.useRef<HTMLDivElement>(null);
 
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["start end", "center center"], // Customize offsets as needed
+    });
+
+    const opacity = useTransform(scrollYProgress, [0, 1], [0, 1]);
+    const y = useTransform(scrollYProgress, [0, 0.2], [50, 1]);
     return (
-
+        <motion.div
+            ref={ref}
+            style={{
+                opacity,
+                y,
+            }}
+        >
         <S.Connect id={"contacts"}>
                 <Container>
                     <FlexWrapper justify={'space-between'} wrap={'wrap'} gap={"20px"}>
@@ -18,7 +33,7 @@ export const Connect: React.FC = () => {
                     </FlexWrapper>
                 </Container>
         </S.Connect>
-
+        </motion.div>
     );
 };
 
--- src/layout/sections/projects/Projects.tsx
+++ src/layout/sections/projects/Projects.tsx
@@ -5,18 +5,33 @@ import {StyledDescription} from 'src/components/description/StyledDescription';
 import {SectionTitle} from 'src/components/sectionTitle/SectionTitle';
 import {FlexWrapper} from 'src/components/wrapper/FlexWrapper';
 import {S} from './Projects_Styles'
+import { motion, useScroll, useTransform} from 'framer-motion';
 
 export const Projects: React.FC = () => {
+    const ref = React.useRef(null);
+    const { scrollYProgress } = useScroll({ target: ref, offset: ["start end", "center center"] });
+
+    const opacity = useTransform(scrollYProgress, [0, 1], [0, 1]);
+    const y = useTransform(scrollYProgress, [0, 1], [50, 0]);
   return (
-    <S.Projects id={"works"}>
-      <Container>
-        <FlexWrapper direction={"column"} justify={"center"} align={"center"}>
-          <SectionTitle>Projects</SectionTitle>
-          <StyledDescription>Things I’ve built so far</StyledDescription>
-          <Cards/>
-        </FlexWrapper>
-      </Container>
+      <motion.div
+          ref={ref}
+          style={{
+                  opacity,
+                  y,
+                  transition: "all 0.8s ease-out", // Smooth transition for all properties
+              }}
+      >
+        <S.Projects id={"works"}>
+          <Container>
+            <FlexWrapper direction={"column"} justify={"center"} align={"center"}>
+              <SectionTitle>Projects</SectionTitle>
+              <StyledDescription>Things I’ve built so far</StyledDescription>
+              <Cards/>
+            </FlexWrapper>
+          </Container>
     </S.Projects>
+      </motion.div>
   );
 };
 
